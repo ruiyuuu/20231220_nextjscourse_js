cb8d4e732dc6b58743abbbc084096f21bab7fde6
ruiyuuu <jasmine59699@gmail.com>
2023-12-22 12:00:34 +0800

    update

--- app/_document.js
+++ app/_document.js
@@ -1,21 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="zh-Hant-TW">
+      <Head/>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
-// import Document, { Html, Head, Main, NextScript } from 'next/document'
- 
-// class MyDocument extends Document {
-//   //...
-// }
- 
-// export default MyDocument
