6f33bcca82d12a1ec25405ca34c644ceeee2a469
ruiyuuu <jasmine59699@gmail.com>
2023-12-27 09:52:01 +0800

    update

--- app/ui/invoices/create-form.jsx
+++ app/ui/invoices/create-form.jsx
@@ -69,12 +69,14 @@ export default function Form({ customers }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
-          {state.errors?.amount &&
-            state.errors.amount.map((error) => (
-              <p className="mt-2 text-sm text-red-500" key={error}>
-                {error}
-              </p>
-            ))}
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Status */}
